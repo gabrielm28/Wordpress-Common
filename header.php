86be43d8c47245f5dc2b91bbcf83849e526c4c43
Gabriel Martins <gabrielm28.dev@gmail.com>
2019-01-31 19:24:54 -0500

    tudo

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Gabriel Martins Portfolio - The New York Times</title>
+</head>
+<body>
+<header>
+	<nav></nav>
+	<div class="logo"></div>
+	<div class="login"></div>
+</header>
