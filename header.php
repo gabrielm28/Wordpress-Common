543aaaf1b45c95fcdf94477519dc8bd42f5e8f03
Gabriel Martins <gabrielm28.dev@gmail.com>
2019-01-31 20:30:24 -0500

    firsts changes

--- header.php
+++ header.php
@@ -3,10 +3,14 @@
 <head>
 	<meta charset="utf-8">
 	<title>Gabriel Martins Portfolio - The New York Times</title>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url') ?>" ?>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <header>
-	<nav></nav>
+	<button><i class="fas fa-align-justify"></i>
 	<div class="logo"></div>
-	<div class="login"></div>
+	<button>
 </header>
+
+<p class="titulo">Olá mundo</p>
